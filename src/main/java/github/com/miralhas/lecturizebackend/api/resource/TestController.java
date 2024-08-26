0d07ff431b018c6b301009e417d0935974eac871
Miralhas <victor2.miralhas@gmail.com>
2024-08-25 21:19:02 -0300

    fix: Correct NGinx Dockerfile

---
+++ src/main/java/github/com/miralhas/lecturizebackend/api/resource/TestController.java
@@ -0,0 +1,21 @@
+package github.com.miralhas.lecturizebackend.api.resource;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping
+    public String lecturizeIt() throws UnknownHostException {
+        return InetAddress.getLocalHost().toString();
+    }
+}
