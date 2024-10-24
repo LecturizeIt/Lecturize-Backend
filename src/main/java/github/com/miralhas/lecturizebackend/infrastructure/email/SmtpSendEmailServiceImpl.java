df738438d89f22bfef19fb56aebf3bfb67afafaa
Miralhas <victor2.miralhas@gmail.com>
2024-10-23 21:28:16 -0300

    fix: logo not showing on email

--- src/main/java/github/com/miralhas/lecturizebackend/infrastructure/email/SmtpSendEmailServiceImpl.java
+++ src/main/java/github/com/miralhas/lecturizebackend/infrastructure/email/SmtpSendEmailServiceImpl.java
@@ -6,6 +6,7 @@ import github.com.miralhas.lecturizebackend.domain.service.SendEmailService;
 import github.com.miralhas.lecturizebackend.infrastructure.exception.EmailException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -23,16 +24,16 @@ public class SmtpSendEmailServiceImpl implements SendEmailService  {
         try {
             String body = processTemplate(message);
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
             helper.setFrom("lecturizeit");
             helper.setText(body, true);
             helper.setSubject(message.getSubject());
             helper.setTo(message.getRecipients().toArray(new String[]{}));
+            helper.addInline("logo", new ClassPathResource("static/images/logo.png"));
             javaMailSender.send(mimeMessage);
         } catch (Exception e) {
             throw new EmailException("Não foi possível enviar o e-mail", e);
         }
-
     }
 
     public String processTemplate(Message message) {
