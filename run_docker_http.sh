24ab7b470134a4120241c652ed6c78ad84719cd8
Miralhas <victor2.miralhas@gmail.com>
2025-03-28 18:17:40 +0000

    feat: add user claim to jwt

---
+++ run_docker_http.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to the current directory (just to be sure)
+cd "$(dirname "$0")"
+
+# Run Maven clean install, skipping tests
+echo "Running Maven clean install..."
+./mvnw clean install -DskipTests
+
+# Set environment variables and run docker-compose
+echo "Setting environment variables and starting Docker containers..."
+# export MYSQL_DATABASE="lecturizeit"
+# export MYSQL_PASSWORD="admin"
+docker compose -f compose.yaml -f compose.dev.yaml up -d --build --force-recreate
+
+echo "Script completed."
